80419db9010bce5715979dd128d3f9fa90e968dc
Viktoriia Badisova <vbadisova@gmail.com>
2018-05-25 13:01:37 +0300

    localize segmented control in Spanish

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -39,7 +39,11 @@ class MapViewController: UIViewController {
     // MARK: - Configure UI
     
     func configureSegmentedControl() {
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        let standardString = NSLocalizedString("Standard", comment: "Standard map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let satelliteString = NSLocalizedString("Satellite", comment: "Satellite map view")
+        
+        let segmentedControl = UISegmentedControl(items: [standardString, hybridString, satelliteString])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
